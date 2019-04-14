81486fd5976c3ad7dc87ef334c512bd1a37c62f0
bmcmahen <ben.mcmahen@gmail.com>
2019-04-13 20:48:41 -0700

    improve gatsby remark compression

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,8 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1200,
+              quality: 90,
+              withWebp: true,
             },
           },
           {
